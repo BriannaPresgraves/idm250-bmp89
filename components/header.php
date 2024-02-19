392587ae4e36c896da8382e59af91e3afd7f4b84
Brianna Presgraves <bmp89@drexel.edu>
2024-02-19 11:45:47 -0500

    update header

--- components/header.php
+++ components/header.php
@@ -1,5 +1,6 @@
 <header class="bg-cyan-100 px-4 py-2">
-        <h1>Crafted Charm</h1>
+
+<img class="header-logo" src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/logo.png" alt="logo">
 
         <nav class="text-right lg:gap-x-12">
 
