a3b5e2a0000e91ba1ca394a6fbe66773b7c6c14e
Brianna Presgraves <bmp89@drexel.edu>
2024-02-26 17:01:37 -0500

    404 update

--- 404.php
+++ 404.php
@@ -1,12 +1,18 @@
 <?php get_header('simple'); ?>
-<h1 class="text-xl">
-    <?php echo get_the_title(); ?>
-</h1>
 
-<p> 404 </p>
-
-<?php wp_nav_menu([
-            'theme_location' => '404-menu'
-        ]); ?>
-
+<div>
+  <div class="mx-auto w-full max-w-7xl px-6 pt-10 pb-16 sm:pb-24 lg:px-8">
+    <div class="mx-auto mt-20 max-w-2xl text-center sm:mt-24">
+      <p class="text-9xl font-semibold leading-8 text-indigo-900 mb-10">404</p>
+      <h1 class="mt-4 text-lg font-bold tracking-tight sm:text-5xl">Oops! Page Not found</h1>
+      <p class="mt-4 text-base leading-7 sm:mt-6 sm:text-lg sm:leading-8">The page you are looking for does not exist.</p>
+    </div>
+      <div class="mt-10 flex justify-center">
+        <a href="https://bmp89.net/idm250/" class="text-sm font-semibold leading-6 text-indigo-900">
+          <span aria-hidden="true">&larr;</span>
+          Back to home
+        </a>
+      </div>
+  </div>
+</div>
 <?php get_footer(); ?>
