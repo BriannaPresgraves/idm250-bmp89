50d32c296f99c09e4fda6f3209ab8b2e53a95d5c
Brianna Presgraves <bmp89@drexel.edu>
2024-03-04 19:27:14 -0500

    update

--- page.php
+++ page.php
@@ -1,4 +1,15 @@
-<?php get_header(); ?>
+<?php
+/**
+ * Page Template
+ *
+ * The page.php template is used to display individual pages. This template is called when a single page's content is queried.
+ * It allows for customization of how page content is displayed, separate from posts or custom post types.
+ *
+ * If specific page templates (like front-page.php, or a custom template) are not found, WordPress will use page.php as a fallback.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-page
+ */
+get_header(); ?>
 <h1 class="text-3xl my-8 text-center">
     <?php echo get_the_title(); ?>
 </h1>
--- single.php
+++ single.php
@@ -1,4 +1,17 @@
-<?php get_header(); ?>
+<?php
+/**
+ * Single Post Template
+ *
+ * The single.php template file is responsible for displaying the content of a single post in a WordPress theme.
+ * This file is called when a viewer clicks on a post's title or permalink, leading them to the individual post page.
+ *
+ * If single.php does not exist in your theme, WordPress will use index.php as a fallback to display the single post.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ */
+
+get_header(); ?>
 <h1 class="text-3xl my-8 text-center">
     <?php echo get_the_title(); ?>
 </h1>
